3a20d3f7ea7ee2e4cf68976932bf6dfde78eb362
ndongo716 <70033480+ndongo716@users.noreply.github.com>
2025-04-28 08:58:11 +0000

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  server{
+    host:'0.0.0.0',
+    port:'5173',
+    allowedHosts:["35.180.73.184"]
+  }
 });
